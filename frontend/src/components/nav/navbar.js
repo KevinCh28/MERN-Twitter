cc1b0f30d6afbdc91e7973af4dcfac24e7309684
Kevin Chen <kevinch28@gmail.com>
2022-04-02 17:17:49 -0400

    add nav css

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -18,7 +18,7 @@ class NavBar extends React.Component {
   getLinks() {
     if (this.props.loggedIn) {
       return (
-        <div>
+        <div className="nav-bar-container">
           <Link to={'/tweets'}>All Tweets</Link>
           <Link to={'/profile'}>Profile</Link>
           <Link to={'/new_tweet'}>Write a Tweet</Link>
@@ -27,7 +27,7 @@ class NavBar extends React.Component {
       );
     } else {
       return (
-        <div>
+        <div className="nav-bar-container">
           <Link to={'/signup'}>Signup</Link>
           <Link to={'/login'}>Login</Link>
         </div>
